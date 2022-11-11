6ea574657291760f97735c778f93ddcb5e34fcd4
Arnab Roy <arnabroy@chegg.com>
2022-11-11 11:00:23 +0530

    new changes for more 02

--- src/utils/moeng.utils.ts
+++ src/utils/moeng.utils.ts
@@ -4,18 +4,18 @@ import { User } from "../types";
 export const Moengage: any = window.Moengage;
 
 export const loginMoengageSession = async (user: User): Promise<boolean> => {
-    return new Promise<boolean>((resolve) => {
+    return new Promise<boolean>(async (resolve) => {
         console.log('loginMoengageSession called');
         try {
             if (!Moengage) throw new Error('Moengage not found!');
 
-            Moengage.add_unique_user_id(user.id);
-            Moengage.add_email(user.email);
-            Moengage.add_user_name(user.email);
-            Moengage.add_first_name(user.firstName);
-            Moengage.add_last_name(user.lastName);
-            Moengage.add_mobile(user.phone);
-            Moengage.add_user_attribute('user-type', user.userType);
+            await Moengage.add_unique_user_id(user.id);
+            await Moengage.add_email(user.email);
+            await Moengage.add_user_name(user.email);
+            await Moengage.add_first_name(user.firstName);
+            await Moengage.add_last_name(user.lastName);
+            await Moengage.add_mobile(user.phone);
+            await Moengage.add_user_attribute('user-type', user.userType);
 
             console.log('loginMoengageSession done');
             resolve(true);
